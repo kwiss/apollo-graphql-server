f28c36fd719fcab2186d9da655c1737e97f7c0ab
DUMONT Christophe <ext-cdumont@lesechos.fr>
2019-02-28 17:49:36 +0100

    initial commit

---
+++ start/server/src/index.js
@@ -0,0 +1,37 @@
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./schema');
+const LaunchAPI = require('./datasources/launch');
+const UserAPI = require('./datasources/user');
+const resolvers = require('./resolvers');
+const { createStore } = require('./utils');
+const isEmail = require('isemail');
+
+const store = createStore();
+
+const dataSources = () => ({
+  launchAPI: new LaunchAPI(),
+  userAPI: new UserAPI({ store }),
+});
+
+const server = new ApolloServer({
+  context: async ({ req }) => {
+    // simple auth check on every request
+    const auth = (req.headers && req.headers.authorization) || '';
+    const email = Buffer.from(auth, 'base64').toString('ascii');
+
+    // if the email isn't formatted validly, return null for user
+    if (!isEmail.validate(email)) return { user: null };
+    // find a user by their email
+    const users = await store.users.findOrCreate({ where: { email } });
+    const user = users && users[0] ? users[0] : null;
+
+    return { user: { ...user.dataValues } };
+  },
+  typeDefs,
+  resolvers,
+  dataSources,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
